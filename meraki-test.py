d674980d07b26bea20d095d40ef31357c92fcf02
Steven Coutts <stevec@couttsnet.com>
2022-08-17 09:04:56 +0100

    Update

--- meraki-test.py
+++ meraki-test.py
@@ -1,11 +1,10 @@
 import requests
 import json
 
-# Set api key. The below api key is for the Meraki Sandbox
-# Replace it with your API key
+# Set api key.
 api_key = '9c1196c0836dcd0ce0e3cdc3fc5615c29e54fd98'
 
-# Set header parameters
+# Set header
 headers = {
    "X-Cisco-Meraki-API-Key" : api_key
 }
@@ -19,7 +18,7 @@ response = requests.request(
    headers=headers
 )
 
-# Print organization id and name
+# Print organization output
 my_orgs = json.loads(response.text)
 for org in my_orgs:
     print (org)
