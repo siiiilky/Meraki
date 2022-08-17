537b432ee763ef2ef9456c8ee36501eee1d8e739
Steven Coutts <stevec@couttsnet.com>
2022-08-17 11:18:29 +0100

    Move more stuff to config file

--- meraki-test.py
+++ meraki-test.py
@@ -2,18 +2,11 @@ import requests
 import json
 import config
 
-# Set header
-headers = {
-   "X-Cisco-Meraki-API-Key": config.APIKEY
-}
-
-url = "https://api.meraki.com/api/v1/organizations"
-
 # Send request and get response
 response = requests.request(
    "GET",
-   url,
-   headers=headers
+   config.url,
+   headers=config.headers
 )
 
 # Print organization output
