dfb8e640867af46d4a1874ddf5305810e35b3606
Steven Coutts <stevec@couttsnet.com>
2022-08-17 11:16:45 +0100

    Move API Key to config file and regenerate new one so the real key isn't in Git anymore :)

--- meraki-test.py
+++ meraki-test.py
@@ -1,12 +1,10 @@
 import requests
 import json
-
-# Set api key.
-api_key = '9c1196c0836dcd0ce0e3cdc3fc5615c29e54fd98'
+import config
 
 # Set header
 headers = {
-   "X-Cisco-Meraki-API-Key": api_key
+   "X-Cisco-Meraki-API-Key": config.APIKEY
 }
 
 url = "https://api.meraki.com/api/v1/organizations"
