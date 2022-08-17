2f9cc2fa4371b636eddd443eb3881f676f938a3b
Steven Coutts <stevec@couttsnet.com>
2022-08-17 09:08:27 +0100

    Update

--- meraki-test.py
+++ meraki-test.py
@@ -6,7 +6,7 @@ api_key = '9c1196c0836dcd0ce0e3cdc3fc5615c29e54fd98'
 
 # Set header
 headers = {
-   "X-Cisco-Meraki-API-Key" : api_key
+   "X-Cisco-Meraki-API-Key": api_key
 }
 
 url = "https://api.meraki.com/api/v1/organizations"
@@ -21,4 +21,4 @@ response = requests.request(
 # Print organization output
 my_orgs = json.loads(response.text)
 for org in my_orgs:
-    print (org)
+    print(org)
